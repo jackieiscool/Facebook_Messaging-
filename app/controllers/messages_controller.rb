237b8957aa9b3c8a4afff7ebbec4ebfe19915e2e
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2012-08-25 17:45:12 -0700

    Creates message objects

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,28 @@
 class MessagesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(params[:message])
+    if @message.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
