1cec7e06da17440b22e0b291ea81a982e8564d7f
Jacqueline Herrlin <j.m.herrlin@gmail.com>
2012-08-27 14:08:40 -0700

    Uninstalled bootstrap

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,6 +2,9 @@ class HomepageController < ApplicationController
   prepend_before_filter :check_for_authorization
 
   def index
+    @user = FbGraph::User.me(facebook_token)
+    @user.fetch
     @user_info = current_user
+    @friends = @user.friends
   end
 end
